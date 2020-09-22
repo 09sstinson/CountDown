d1f71a8355959a012f93a601706a4eb9d0987747
09sstinson <09sstinson@gmail.com>
2020-09-22 01:38:06 +0100

    second commit

--- src/main/java/com/sstinson/countdown/Main.java
+++ src/main/java/com/sstinson/countdown/Main.java
@@ -1,5 +1,8 @@
 package com.sstinson.countdown;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.Stack;
 
 public class Main {
@@ -14,15 +17,102 @@ public class Main {
         System.out.println(stack.get(0));
     }
 
+    public int indexOfNthOccurrence(String string, String regex, int n){
+        if(n> string.length()){
+            return -1;
+        }
+        int tempIndex = -1;
+        for(int i = 0; i < n ; i++ ){
+            tempIndex = string.indexOf(regex, tempIndex + 1 );
+        }
+        return tempIndex;
+    }
+
+    public String replaceCharAtIndexWithString(String string, String replacement, int index){
+        return string.substring(0,index) + replacement + string.substring(index+1);
+    }
+
+    public boolean checkBinaryBrackets(String s){
+        if(!checkBalancedBrackets(s)){
+            return false;
+        }
+        String temp = s;
+
+        while(temp != temp.replaceAll("\\(..\\)","x") ){
+            temp = temp.replaceAll("\\(..\\)","x");
+        }
+
+        return temp.equals("x") || temp.equals("xx");
+    }
+
+    public ArrayList<String> generateAllBinaryBrackets(){
+        ArrayList<String> binaryBrackets = new ArrayList<String>();
+        return null;
+    }
+
+    public ArrayList<String>  generateAllPossibleReplacements(String string, String orig, String regex){
+        int n=1;
+        ArrayList<String> replacements = new ArrayList<String>();
+        int index;
+        while( (index = indexOfNthOccurrence(string, orig,n++)) != -1){
+            replacements.add(replaceCharAtIndexWithString(string,regex,index));
+
+        }
+        return replacements;
+    }
+
+    public ArrayList<String> generateAllPossibleReplacements(ArrayList<String> strings, String orig, String regex){
+        ArrayList<String> replacements = new ArrayList<String>();
+        for(String s : strings){
+            replacements.addAll(generateAllPossibleReplacements(s, orig, regex));
+        }
+        return replacements;
+    }
+
+    public ArrayList<String> removeDuplicates(ArrayList<String> list)
+    {
+
+        // Create a new LinkedHashSet
+        Set<String> set = new LinkedHashSet<>();
+
+        // Add the elements to set
+        set.addAll(list);
+
+        // Clear the list
+        list.clear();
+
+        // add the elements of set
+        // with no duplicates to the list
+        list.addAll(set);
+
+        // return the list
+        return list;
+    }
+
+    public ArrayList<String> generateAllBinaryBrackets(int length){
+        if(length<=2){
+            return null;
+        }
+        String s = "xx";
+        ArrayList<String> list;
+        list = generateAllPossibleReplacements(s,"x","(xx)");
+        list = removeDuplicates(list);
+        for(int i = 3; i < length; i++){
+
+            list = generateAllPossibleReplacements(list,"x","(xx)");
+            list = removeDuplicates(list);
+        }
+
+        return list;
+    }
+
     public boolean checkBalancedBrackets(String s){
         Stack a = new Stack();
-        char top = '0';
-        //boolean flag = false;
+        char top;
         for(int i =0; i < s.length(); i++){
 
             if(s.charAt(i) == '('){
                 a.push('(');
-                //flag = true;
                 continue;
             }
 
--- src/test/java/countdownTest.java
+++ src/test/java/countdownTest.java
@@ -1,6 +1,8 @@
 import com.sstinson.countdown.Main;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class countdownTest {
@@ -9,11 +11,51 @@ public class countdownTest {
     @Test
     public void testCheckBalancedBrackets(){
         String s1 = ")()";
-        String s2 = "((a)x(1x1)(12))";
+        String s2 = "((xx)(xx)(xx))";
 
         assertTrue(m.checkBalancedBrackets(s2));
-        //assertFalse(m.checkBalancedBrackets(s1));
+
     }
 
+    @Test
+    public void testCheckBinaryBrackets(){
+        String s2 = "((a(bc))(de))";
+        assertTrue(m.checkBinaryBrackets(s2));
+    }
 
+    @Test
+    public void testIndexOfNthOccurrence(){
+        String s = "aaaaa";
+        assertEquals(m.indexOfNthOccurrence(s,"a",4),3);
+        String s2 = "(xx)x(xx)";
+        assertEquals(m.indexOfNthOccurrence(s2,"x",4),6);
+
+    }
+
+    @Test
+    public void testReplaceCharAtIndexWithString(){
+        String s2 = "(xx)x(xx)";
+        assertEquals(m.replaceCharAtIndexWithString(s2,"(xx)",2),"(x(xx))x(xx)");
+    }
+
+    @Test
+    public void testGenerateAllPossibleReplacementsOfChar(){
+        String s2 = "(xx)x";
+        System.out.println(m.generateAllPossibleReplacements(s2,"x","(xx)"));
+    }
+
+    @Test
+    public void testGenerateAllPossibleReplacementsOfCharArrayList(){
+        String s2 = "xx";
+        ArrayList<String> list = m.generateAllPossibleReplacements(s2,"x","(xx)");
+        System.out.println(list = m.generateAllPossibleReplacements(list,"x","(xx)"));
+        System.out.println(m.removeDuplicates(list));
+    }
+
+    @Test
+    public void testGenerateAllBinaryBrackets(){
+        System.out.println(m.generateAllBinaryBrackets(6));
+        assertEquals(m.generateAllBinaryBrackets(6).size(),42);
+
+    }
 }
